3974031d8d8d9d68174f29b8a753aa43f0e88b3d
rsrhere <rahulrathore355113@gmail.com>
2024-12-01 22:19:42 +0530

    correcting the previous push

---
+++ Web-Dev/week4/local_project/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.red('This text will be red'));
+console.log(chalk.blue.bold('This text will be blue and bold'));
+console.log(chalk.yellow.underline('This text will be green and underlined'));
